cf81a75afff8b127370c03e3ab9ee20290c8463b
Inmobi Korea <taki1984@naver.com>
2018-11-12 15:32:08 +0900

    # add devices app && settings

--- inmobi/config/urls.py
+++ inmobi/config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path("test/", include("oneonone.urls", namespace="test")),
+    path("devices/", include("devices.urls", namespace="devices")),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
