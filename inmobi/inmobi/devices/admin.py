777d72f3e37c97e64a17117a0dd48316e5d64efa
Inmobi Korea <taki1984@naver.com>
2018-11-12 17:12:32 +0900

    # create device models

--- inmobi/inmobi/devices/admin.py
+++ inmobi/inmobi/devices/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Device, DeviceAdId
 
-# Register your models here.
+@admin.register(Device)
+class devicesAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'device_name', 'phone_number')
+
+
+@admin.register(DeviceAdId)
+class deviceAdIdAdmin(admin.ModelAdmin):
+    list_display = ('device', 'adid')
--- inmobi/inmobi/devices/models.py
+++ inmobi/inmobi/devices/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from ..oneonone import models as oneonone_models
 
-# Create your models here.
+
+class Device(oneonone_models.TimeStampedModel):
+
+    OS_CHOICES = (
+        ('ios', 'iOS'),
+        ('aos', 'AOS')
+    )
+
+    owner = models.CharField(max_length=100)
+    device_name = models.CharField(max_length=50)
+    owner = models.CharField(max_length=50)
+    model_name = models.CharField(max_length=200)
+    os = models.CharField(max_length=30, choices=OS_CHOICES)
+    os_version = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}의 {}'.format(self.owner, self.device_name)
+
+
+class DeviceAdId(oneonone_models.TimeStampedModel):
+    device = models.ForeignKey(Device, on_delete=models.PROTECT)
+    adid = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return '{}의 {}'.format(self.device.owner, self.device.device_name)
+
+
+
+
